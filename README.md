3f92d1a3ae807e0d08bbf65c09b3ab977aa5b078
brendena <cerebralasylum1@gmail.com>
2023-01-03 19:16:06 -0500

    changed picoCode.py to main.py

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ The goal is to be able to measure the power consumption of my house and send tha
 
 ## Setup
 * [load micropython onto the pico](https://www.raspberrypi.com/documentation/microcontrollers/micropython.html)
+
+You'll then load the software onto the pico.  With the thorny IDE you can go to \[File\]->\[Save as\] and then select pico as your location.
 * Add ads115.py as a file to your pico
 * Add consts.py as a file to your pico.  This file will have you wifi passwords and mqtt settings in it.
+* Add main.py to your pico.  This one will auto boot when you plug it in
 
 # support lib
-* [ADS1115] (https://github.com/robert-hh/ads1x15/blob/master/ads1x15.py) included the original source to be thorough 
+* [ADS1115](https://github.com/robert-hh/ads1x15/blob/master/ads1x15.py) included the original source to be thorough 
--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from consts import networkID,networkPassword, mqtt_server,client_id,topic_pub,po
 from time import sleep_ms, ticks_ms, ticks_us
 
 
-
-
-#connect up to wifi
+##########################
+####Setting up WIFI#######
+##########################
 print("tring to connect to " + networkID)
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -28,7 +28,9 @@ while(not wlan.isconnected()):
     wlan.connect(networkID,networkPassword)
 
         
-#connect up to MQTT
+##########################
+#Connecting to MQTT#######
+##########################
 def mqtt_connect():    
     client = MQTTClient(client_id, mqtt_server,user="brenden", password="password", keepalive=3600)
     client.connect()
@@ -46,8 +48,12 @@ try:
 except OSError as e:
     reconnect()
     
-    
-ADC_RATE = 3 # delay rate in milliseconds
+
+##########################
+########ADC Reads#########
+##########################
+
+ADC_RATE = 5 # delay rate in milliseconds
 
 class ADC:
     def __init__(self, ADS,maxCurrent,maxVoltage, channel1, channel2 = None):
@@ -89,11 +95,15 @@ class MQTT_adc:
         if(kWats < 0.0005):
             kWats = 0
         kWatsString = bytearray(str(kWats).encode('utf-8'))
-        #self.mqttClient.publish(self.topic, kWatsString)
+        self.mqttClient.publish(self.topic, kWatsString)
 
         print(kWatsString)
         
-        
+
+
+##########################
+#Putting it all together##
+##########################
 i2c = machine.I2C(1, scl=machine.Pin(15), sda=machine.Pin(14))
 
 #TED clamps 4.096 200 amp 
@@ -103,17 +113,17 @@ Ted2 = ADC(TedADS,200,3, 1)
 
 #YMDC clamps 4.096 100 amp 
 YMDC_ADS = ADS1115(i2c,address=0x49, gain=1)
-YMDC1 = ADC(YMDC_ADS,100,3.3,0,1)
-YMDC2 = ADC(YMDC_ADS,100,3.3,2,3) 
+YMDC1 = ADC(YMDC_ADS,100,3,0,1)
+YMDC2 = ADC(YMDC_ADS,100,3,2,3) 
 
 
 
 
 listAdc = [
-    MQTT_adc(client,Ted1, topic_pub + "/leftMain"),
+    #MQTT_adc(client,Ted1, topic_pub + "/leftMain"),
     MQTT_adc(client,Ted2, topic_pub + "/rightMain"),
     MQTT_adc(client,YMDC1, topic_pub + "/leftGarage"),
-    MQTT_adc(client,YMDC2, topic_pub + "/rightGarage")
+    #MQTT_adc(client,YMDC2, topic_pub + "/rightGarage")
 ] 
 
 while(1):
